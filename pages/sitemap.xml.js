d7bb2c5431d83bca309440cde4f77936ce0551cd
shreyash <shreyashmeshram76@gmail.com>
2025-08-04 21:23:51 +0530

    uninstall react quill and matine

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -2,23 +2,40 @@
 import dbConnect from '../lib/dbConnect';
 import BlogPost from '../models/BlogPost';
 
-export default async (req, res) => {
+export async function getServerSideProps({ res }) {
+  // Connect to MongoDB
   await dbConnect();
-  const posts = await BlogPost.find({published: true});
-  const sitemap = `<?xml version="1.0"?>
+
+  // Fetch published blog posts
+  const posts = await BlogPost.find({ published: true });
+
+  // Build XML Sitemap
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${process.env.NEXT_PUBLIC_BASE_URL}/</loc>
     <priority>1.0</priority>
   </url>
-  ${posts.map(post=>`
+  ${posts
+    .map(
+      (post) => `
   <url>
     <loc>${process.env.NEXT_PUBLIC_BASE_URL}/blog/${post._id}</loc>
     <priority>0.8</priority>
-  </url>`).join('')}
+  </url>`
+    )
+    .join('')}
 </urlset>`;
 
+  // Send XML to browser
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
   res.end();
-};
+
+  return { props: {} }; // Page does not render anything
+}
+
+// This page does not render a component
+export default function Sitemap() {
+  return null;
+}
